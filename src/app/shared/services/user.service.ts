501b9e6593b685fd099925ea79bb53790974df92
YbanezAdrien <adrien.ybanez@gmail.com>
2020-11-26 12:20:14 +0100

    réalisaton de l'exo

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  public users: BehaviorSubject<any> = new BehaviorSubject([]);
+
+  constructor() { }
+
+  public addUser(user) {
+    const tempUsers = this.users.value;
+    tempUsers.push(user);
+    this.users.next(tempUsers);
+  }
+}
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription} from 'rxjs';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   public users: string[];
+  private subscription: Subscription = new Subscription();
 
   constructor(
-    // il faut probablement injecter un service ici !
+    private userService: UserService
   ) { }
 
   ngOnInit() {
-    // il faut initialiser les users ici avec le service
+    this.subscription.add(
+      this.userService.users.subscribe(users => {
+        this.users = users;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
